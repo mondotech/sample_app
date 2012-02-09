c81af1c19e8fb794efe0ef6e973fbbe419824acf
mondotech <github@mondotech.net>
2012-02-08 21:27:30 -0500

    Done with before-each

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe PagesController do
+
   render_views
+
+    before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+    end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -12,7 +17,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -25,8 +30,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -39,10 +43,8 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
-   
   end
 
  describe "GET 'help'" do
@@ -54,9 +56,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Help")
+                                    :content => @base_title + " | Help")
     end
   end
 end
-
