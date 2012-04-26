300fa2a6728d0b011d08998671ec20e57b763b35
mondotech <github@mondotech.net>
2012-04-25 20:27:17 -0400

    Added user following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,23 +12,30 @@ describe PagesController do
 
   describe "GET 'home'" do
 
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
+    describe "when not signed in" do
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", :content => @base_title + " | Home")
+      before(:each) do
+        get :home
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title",
+                                      :content => "#{@base_title} | Home")
+      end
     end
 
     
-    describe "for signed-in users" do 
+    describe "when signed in" do 
 
       before(:each) do 
          @user = test_sign_in(Factory(:user)) 
          @mp1 = Factory(:micropost, :user => @user)
-         
+         other_user = Factory(:user, :email => Factory.next(:email))
+         other_user.follow!(@user)
       end 
 
       it "should have micropost count in sidebar" do 
@@ -38,9 +45,16 @@ describe PagesController do
          get 'home' 
          response.should have_selector("span.microposts", :content => "2 microposts") 
       end 
-       
 
-    end #describe for signed-in users
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end 
+
+    end #describe when signed in
 
     describe "Display delete link" do
 
